877084a6d061a5869e4a4b58cbc21f9d9bb782b9
Saka <email>
2022-12-10 15:54:55 +0700

    Test Merge Request

---
+++ resources/views/niyo.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NIYO</title>
+</head>
+<body>
+    <h1>HALO SAYA NIYO</h1>
+</body>
+</html>
